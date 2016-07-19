a352f5fbc1c08d26dd1362078968910d497d4f64
Brian Simmons <bsimmonsn@gmail.com>
2016-07-19 16:30:59 -0500

    API: users can now only see their own photos

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -10,7 +10,12 @@ class Api::PhotosController < ApplicationController
   end
 
   def list
-    @photos = Photo.all
+    if doorkeeper_token
+    @current_user = User.find(doorkeeper_token.resource_owner_id)
+    @photos = @current_user.photos
+    else
+      render json: {error: "Could not find"}, status: 422
+    end
   end
 
 end
