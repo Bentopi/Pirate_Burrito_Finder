186e9148db567c1a4c6d90a032e4643843cdb182
Brian Simmons <bsimmonsn@gmail.com>
2016-08-18 14:17:58 -0500

    simplified app for portfolio purposes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,6 +14,7 @@ class WelcomeController < ApplicationController
       url = "https://api.foursquare.com/v2/venues/search?query=burrito&ll=#{lat},#{lon}&client_id=QJNE4GB5I20ZNYEJMY2W0F05BJ2SRFBLI3LV4SXWMG2IRSC4&client_secret=HGZW1QT05VRLK2BKBT1XDRNE5IUI3SRZFGSHPDCD0RGUADOB&v=20160715"
 
       json = JSON.parse(Http.get(url).body)
+
       @venues = json["response"]["venues"]
     end
   end
